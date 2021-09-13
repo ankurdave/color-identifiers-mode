189ed53c5289292181251ecc0ff681a9dad2ede2
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2021-09-13 19:15:01 +0300

    Do not erase user-set timers upon disabling the mode
    The line
    
        (setq color-identifiers:timer nil)
    
    means that if we disable the mode, we erase user set timers.
    
    To avoid that we only cancel the timer, and then when mode is enabled,
    we check whether the timer is present, and if it is whether it needs to
    be activated.

--- color-identifiers-mode.el
+++ color-identifiers-mode.el
@@ -48,6 +48,15 @@
 (defvar color-identifiers:timer nil
   "Timer for running `color-identifiers:refresh'.")
 
+(defun color-identifiers:enable-timer ()
+  (if color-identifiers:timer
+      ;; Someone set the timer. Activate in case we cancelled it.
+      (unless (memq color-identifiers:timer timer-idle-list)
+        (timer-activate-when-idle color-identifiers:timer))
+    (setq color-identifiers:timer
+          (run-with-idle-timer 5 t 'color-identifiers:refresh)))
+  )
+
 ;;;###autoload
 (define-minor-mode color-identifiers-mode
   "Color the identifiers in the current buffer based on their names."
@@ -59,13 +68,10 @@
         (color-identifiers:refresh)
         (add-to-list 'font-lock-extra-managed-props 'color-identifiers:fontified)
         (font-lock-add-keywords nil '((color-identifiers:colorize . default)) t)
-        (unless color-identifiers:timer
-          (setq color-identifiers:timer
-                (run-with-idle-timer 5 t 'color-identifiers:refresh)))
+        (color-identifiers:enable-timer)
         (ad-activate 'enable-theme))
     (when color-identifiers:timer
       (cancel-timer color-identifiers:timer))
-    (setq color-identifiers:timer nil)
     (font-lock-remove-keywords nil '((color-identifiers:colorize . default)))
     (ad-deactivate 'enable-theme))
   (color-identifiers:refontify))
